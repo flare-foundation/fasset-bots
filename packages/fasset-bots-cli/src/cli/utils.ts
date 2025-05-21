495f3119e785ce450370db0b030af59303948380
Iztok <iztok@abelium.com>
2025-05-21 15:15:46 +0200

    log info for each day when exporting logs

--- packages/fasset-bots-cli/src/cli/utils.ts
+++ packages/fasset-bots-cli/src/cli/utils.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import "source-map-support/register";
 
 import { InfoBotCommands } from "@flarenetwork/fasset-bots-core";
-import { blockTimestamp, getOrCreateAsync, isBigNumber, web3DeepNormalize } from "@flarenetwork/fasset-bots-core/utils";
+import { blockTimestamp, DAYS, getOrCreateAsync, isBigNumber, web3DeepNormalize } from "@flarenetwork/fasset-bots-core/utils";
 import { programWithCommonOptions } from "../utils/program";
 import { toplevelRun } from "../utils/toplevel";
 import { validateInteger } from "../utils/validation";
@@ -22,20 +22,28 @@ program
         const bot = await InfoBotCommands.create(secrets, options.config, options.fasset);
         const blockTimestamps = new Map<string, number>();
         validateInteger(blockCount, "blockCount", { min: 1 });
+        let lastPrintTs = 0, count = 0;
+        const startTime = Date.now();
         for await (const event of bot.readAssetManagerLogs(Number(blockCount))) {
             const timestamp = await getOrCreateAsync(blockTimestamps, String(event.blockNumber), (bn) => blockTimestamp(bn));
             const niceArgs = Object.fromEntries(
                 Object.entries(event.args as any)
                     .filter(([k, v]) => !isBigNumber(k) && k !== "__length__")
                     .map(([k, v]) => [k, web3DeepNormalize(v)]));
+            const datetime = new Date(timestamp * 1000);
+            if (timestamp - lastPrintTs >= DAYS) {
+                lastPrintTs = timestamp;
+                console.error(`Exporting date ${datetime.toISOString()}, event count ${count}, elapsed ${(Date.now() - startTime) / 1000}s`);
+            }
             const niceEvent = {
-                datetime: new Date(timestamp * 1000).toISOString(),
+                datetime: datetime.toISOString(),
                 block: Number(event.blockNumber),
                 timestamp: timestamp,
                 name: event.event,
                 args: niceArgs,
             }
             console.log(JSON.stringify(niceEvent));
+            ++count;
         }
     });
 
