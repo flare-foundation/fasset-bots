b64d8f307d043f73c4eb8bed8d96aa5aba5f839b
Urška Krivc <urska.krivc@abelium.com>
2025-07-03 16:58:23 +0200

    revert changes in  scripts

--- packages/fasset-bots-core/scripts/copy-artifacts.sh
+++ packages/fasset-bots-core/scripts/copy-artifacts.sh
@@ -29,26 +29,22 @@ rm -rf artifacts; mkdir -p artifacts
 rm -rf typechain-truffle; mkdir -p typechain-truffle
 
 # copy fasset artifacts
-echo "***** artifacts... "
 cd ${fassetsdir}/artifacts
-gfind -name '*.json' -not -name '*.dbg.json' -not -path './build-info/*' -not -path './cache/*' -not -path './flattened/*' | xargs gcp -t ${projdir}/artifacts --parents
+find -name '*.json' -not -name '*.dbg.json' -not -path './build-info/*' -not -path './cache/*' -not -path './flattened/*' | xargs cp -t ${projdir}/artifacts --parents
 cd ${fassetsdir}
 
 # copy liquidator artifacts
-echo "***** liquidator... "
 cd ${liquidatordir}/artifacts
 mkdir -p ${projdir}/artifacts/liquidator
-gfind -name '*.json' -not -name '*.dbg.json' -path './contracts/*' -not -path './contracts/mock/*' | xargs gcp -t ${projdir}/artifacts/liquidator --parents
+find -name '*.json' -not -name '*.dbg.json' -path './contracts/*' -not -path './contracts/mock/*' | xargs cp -t ${projdir}/artifacts/liquidator --parents
 
 # fix some paths so that sourceName always matches the actual dir
-echo "***** flat... "
 cd ${projdir}
 mkdir -p artifacts/flattened
-# mv artifacts/flare-sc artifacts/flattened/FlareSmartContracts.sol
+mv artifacts/flare-sc artifacts/flattened/FlareSmartContracts.sol
 
 # fix source paths in liquidator jsons
-echo "***** fix... "
-gfind -name '*.json' -path './artifacts/liquidator/*' | xargs gsed -i -e 's/"sourceName": "contracts\//"sourceName": "liquidator\/contracts\//'
+find -name '*.json' -path './artifacts/liquidator/*' | xargs sed -i -e 's/"sourceName": "contracts\//"sourceName": "liquidator\/contracts\//'
 
 # copy some fixed mocks
 cd scripts && cp -r test-mocks/ ../artifacts && cd ..
