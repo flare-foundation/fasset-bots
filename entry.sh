775ad2aa7d873e9d85f6a1838c65d3e2bd483b0e
kuco23 <nseverkar@gmail.com>
2025-08-22 15:11:31 +0200

    fix: simplify entry.sh script and pass sigterm to child processes

--- entry.sh
+++ entry.sh
@@ -11,37 +11,23 @@ fi
 case $1 in
     run-bots)
         echo 'starting back-end and run-agent'
-        if [[ "${LOG_TARGET:-}" != 'console' ]]; then
-            echo 'redirecting logs to file'
-            /bin/sh -c '(yarn start_agent_api > log/agent_ui.log &) && (yarn run-agent > log/agent_run.log &) && (/bin/sh)'
-        else
-            echo 'logs will be printed to console'
-            /bin/sh -c '(yarn start_agent_api &) && (yarn run-agent &) && (/bin/sh)'
-        fi
+        yarn run-agent &
+        yarn start_agent_api &
         ;;
     run-liquidator)
         echo 'starting liquidator'
-        if [[ "${LOG_TARGET:-}" != 'console' ]]; then
-            echo 'redirecting logs to file'
-            /bin/sh -c '(yarn run-liquidator > log/liquidator_run.log &) && (/bin/sh)'
-        else
-            echo 'logs will be printed to console'
-            /bin/sh -c '(yarn run-liquidator &) && (/bin/sh)'
-        fi
+        yarn run-liquidator &
         ;;
     run-challenger)
         echo 'starting challenger'
-        if [[ "${LOG_TARGET:-}" != 'console' ]]; then
-            echo 'redirecting logs to file'
-            /bin/sh -c '(yarn run-challenger > log/challenger_run.log &) && (/bin/sh)'
-        else
-            echo 'logs will be printed to console'
-            /bin/sh -c '(yarn run-challenger &) && (/bin/sh)'
-        fi
+        yarn run-challenger &
         ;;
     *)
-        # The wrong first argument.
         echo "invalid argument: '$1'"
         echo $USAGE_MSG
         exit 1
 esac
+
+trap "echo 'caught SIGTERM, killing children'; kill 0" TERM INT
+
+wait
