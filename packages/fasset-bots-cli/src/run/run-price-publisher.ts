ee142734f2a83f18f51f48a9e2a9c7930f11bf0b
Iztok <iztok@abelium.com>
2025-08-29 15:42:36 +0200

    fixed standalone price-publisher script

--- packages/fasset-bots-cli/src/run/run-price-publisher.ts
+++ packages/fasset-bots-cli/src/run/run-price-publisher.ts
@@ -2,8 +2,8 @@ import "dotenv/config";
 import "source-map-support/register";
 
 import { PricePublisherService } from "@flarenetwork/fasset-bots-core";
-import { Secrets, closeBotConfig, createBotConfig, loadConfigFile } from "@flarenetwork/fasset-bots-core/config";
-import { authenticatedHttpProvider, initWeb3, logger, sleep } from "@flarenetwork/fasset-bots-core/utils";
+import { Secrets, loadConfigFile } from "@flarenetwork/fasset-bots-core/config";
+import { authenticatedHttpProvider, initWeb3, sleep } from "@flarenetwork/fasset-bots-core/utils";
 import { programWithCommonOptions } from "../utils/program";
 import { toplevelRun } from "../utils/toplevel";
 
@@ -16,25 +16,19 @@ program.action(async () => {
     const pricePublisherAddress: string = secrets.required("pricePublisher.address");
     const pricePublisherPrivateKey: string = secrets.required("pricePublisher.private_key");
     await initWeb3(authenticatedHttpProvider(runConfig.rpcUrl, secrets.optional("apiKey.native_rpc")), [pricePublisherPrivateKey], null);
-    const config = await createBotConfig("common", secrets, runConfig, pricePublisherAddress);
-    logger.info(`Asset manager controller is ${config.contractRetriever.assetManagerController.address}.`);
     // start price publisher
     const pricePublisherService = await PricePublisherService.create(runConfig, secrets, pricePublisherAddress);
     pricePublisherService.start();
     // run
-    try {
-        console.log("Price publisher bot started, press CTRL+C to end");
-        const stopBot = () => {
-            console.log("Price publisher bot stopping...");
-            pricePublisherService.requestStop();
-        }
-        process.on("SIGINT", stopBot);
-        process.on("SIGTERM", stopBot);
-        while (!pricePublisherService.stopped) {
-            await sleep(1000);
-        }
-    } finally {
-        await closeBotConfig(config);
+    console.log("Price publisher bot started, press CTRL+C to end");
+    const stopBot = () => {
+        console.log("Price publisher bot stopping...");
+        pricePublisherService.requestStop();
+    }
+    process.on("SIGINT", stopBot);
+    process.on("SIGTERM", stopBot);
+    while (!pricePublisherService.stopped) {
+        await sleep(1000);
     }
     console.log("Price publisher bot stopped");
 });
